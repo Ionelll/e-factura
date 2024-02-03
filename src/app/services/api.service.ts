c9de87ec8acbf64826e0f00131e1bc0144d1428b
Ionelll <serban.nelutu@gmail.com>
2024-02-04 00:13:42 +0100

    added api link to calls

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { User } from '../models/user.model';
-import { environment } from '../../../environment';
 import { Company } from '../models/company.model';
 
 @Injectable({ providedIn: 'root' })
@@ -15,7 +14,9 @@ export class ApiService {
   setCustomerNamesList(value: string) {
     console.log('hi');
     this.http
-      .get<string[]>(`${environment.api_url}/searchcustomer/${value}`)
+      .get<string[]>(
+        `https://invoicefree-backend.onrender.com/searchcustomer/${value}`
+      )
       .subscribe((res) => {
         console.log(res);
         this.customerList.next(res);
@@ -27,7 +28,9 @@ export class ApiService {
 
   setCustomer(value: string) {
     this.http
-      .get<Company>(`${environment.api_url}/returncustomer/${value}`)
+      .get<Company>(
+        `https://invoicefree-backend.onrender.com/returncustomer/${value}`
+      )
       .subscribe((res) => {
         this.customer.next(res);
       });
