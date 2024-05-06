6b0d9c0f1ad3d06e270b1fb4d5d90e7a7808f022
Ionelll <serban.nelutu@gmail.com>
2024-05-06 11:00:54 +0200

    removed unnessecary files

--- environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  api_url: 'https://invoicefree-backend-production.up.railway.app/api',
-  locationUrl:
-    'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=',
-  placesKey: 'AIzaSyBA1zSsfrFni-rUTWZz3okBiUaaZGTIB0Y',
-};
